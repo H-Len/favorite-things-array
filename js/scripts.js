d30603257a02fc1962878b22335be0c9122918fc
hannah melendy and jaime takikawa <hannah@>
2019-04-01 09:48:17 -0700

    add code in html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("button#add1").click(function( {
+    var favorite = $("add1").append();
+  };
+  $("button#add2").click(function( {
+    favorite = $("add2").append();
+  };
+  $("button#add3").click(function( {
+    favorite = $("add3").append();
+  };
+  favorite.show();
+  event.preventDefault();
+
+});
