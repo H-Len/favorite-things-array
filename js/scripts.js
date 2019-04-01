bf608828dd7141578cee1b47237836528e11f326
hannah melendy and jaime takikawa <hannah@>
2019-04-01 14:04:45 -0700

    try to use .append with an array from a form

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(document).ready(function() {
-  $(".form").submit(function(event){
-    event.preventDefault();
-    var favorites = [];
-    var favoriteFood = $("#add1").val();
-    var favoriteMovie = $("#add2").val();
-    var favoriteBook = $("#add3").val();
-
-    console.log(favoriteFood);
-    favorites.push(favoriteFood, favoriteMovie, favoriteBook);
-    console.log(favorites);
-
-    document.getElementById("add1").innerHTML = favorites[0];
-
-    $("favorites").show();
+// $(document).ready(function() {
+//   $(".form").submit(function(event){
+//     event.preventDefault();
+    // var favorites = [];
+    // var favoriteFood = $("#add1").val();
+    // var favoriteMovie = $("#add2").val();
+    // var favoriteBook = $("#add3").val();
+//
+//     console.log(favoriteFood);
+//     favorites.push(favoriteFood, favoriteMovie, favoriteBook);
+//     console.log(favorites);
+//
+//     document.getElementById("add1").innerHTML = favorites[0];
+//
+//     $("favorites").show();
 
     // <ul>
     //   <li>"add1"</li>
@@ -26,5 +26,28 @@ $(document).ready(function() {
   // alert("Why is this not working");
   //  favorite = add1.append("#add3");
   //  favorite.show();
- });
+//  });
+// });
+//
+// $(document).ready(function() {
+//   $(".form").submit(function(event) {
+//     var favoritesInput = ["add1", "add2", "add3"];
+
+
+$(document).ready(function() {
+  $(".form").submit(function(event){
+    var favorites = ["add1", "add2", "add3"];
+    favorites.forEach(function(favorite) {
+      var userInput = $("input" + favorites).val();
+
+      console.log("try one");
+      $("." + favorite).text(userInput);
+      $(".favoriteList").append("li" + favorites + "/li");
+      $("#favoritesInput").show();
+      console.log("woohoo");
+      event.preventDefault();
+    });
+
+
+  });
 });
