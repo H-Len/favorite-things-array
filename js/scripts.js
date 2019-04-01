1cec7cb2d7192c66aa85ab9f6bfaf33ec4e38a96
hannah melendy and jaime takikawa <hannah+jaime@>
2019-04-01 12:00:32 -0700

    document further attempts to display favorites on the webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 $(document).ready(function() {
-  $("button#add1").click(function( {
-    var favorite = $("add1").append();
-  };
-  $("button#add2").click(function( {
-    favorite = $("add2").append();
-  };
-  $("button#add3").click(function( {
-    favorite = $("add3").append();
-  };
-  favorite.show();
-  event.preventDefault();
+  $(".form").submit(function(event){
+    event.preventDefault();
+    var favorites = [];
+    var favoriteFood = $("#add1").val();
+    var favoriteMovie = $("#add2").val();
+    var favoriteBook = $("#add3").val();
 
+    console.log(favoriteFood);
+    favorites.push(favoriteFood, favoriteMovie, favoriteBook);
+    console.log(favorites);
+
+    document.getElementById("add1").innerHTML = favorites[0];
+
+    $("favorites").show();
+
+    // <ul>
+    //   <li>"add1"</li>
+    //   <li>"add2"</li>
+    //   <li>"add3"</li>
+    // </ul>
+
+
+  //   console.log(favorite);
+  //   favorite = add1.append("#add2");
+  // alert("Why is this not working");
+  //  favorite = add1.append("#add3");
+  //  favorite.show();
+ });
 });
